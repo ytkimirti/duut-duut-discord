f837b5371a051361cecb5617ecb2e55ff1323bc2
Yusuf <yusuftaha9@gmail.com>
2022-07-22 01:00:34 +0300

    Fix auth thing

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,19 +11,33 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# So this is the duut-duut/server/ dir
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+dotenv_file = os.path.join(BASE_DIR.parent, ".env")
+if os.path.isfile(dotenv_file):
+    load_dotenv(dotenv_file)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED***'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == "True"
+
+# - My settings
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media/'
+
+# - End of my settings
 
 ALLOWED_HOSTS = []
 
