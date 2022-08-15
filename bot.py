b1c55f726ecc650c4be85f578271fa75cfbae141
Yusuf <yusuftaha9@gmail.com>
2022-08-16 00:16:25 +0300

    New images thing

--- bot.py
+++ bot.py
@@ -1,20 +1,33 @@
 import discord
 import os
 from discord.ext import commands
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import reader
 
-load_dotenv()
+# load_dotenv()
 
 intents = discord.Intents.default()
 # intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.command()
-async def duut(ctx):
-    result = reader.read("./test.png")
+async def duut(ctx, arg):
+    if arg:
+        result = reader.read(f"./images/{arg}")
+    else:
+        result = reader.read("./test.png")
+
+    await ctx.send('\n'.join(result))
+
+@bot.command()
+async def duutxl(ctx, arg):
+    if arg:
+        result = reader.read(f"./images/{arg}")
+    else:
+        result = reader.read("./test.png")
 
     for str in result:
         await ctx.send(str)
 
-bot.run(os.getenv('TOKEN'))
+# bot.run(os.getenv('TOKEN'))
+bot.run("OTk5NzU2OTQzNjAwMTQ0Mzg0.GH0saF.YDNDkAmpzZShDRsUemZTu82pLr5MhEMAe5cNpw")
